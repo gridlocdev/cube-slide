c78a725d0606c05b1b69a2e141c8f4cef7e2e788
gridlocdev <gridlocdev@fastmail.com>
2026-03-13 23:52:14 -0700

    fix speed and physics of ball

--- godot-version/scripts/ball_obstacle.gd
+++ godot-version/scripts/ball_obstacle.gd
@@ -1,17 +1,22 @@
 extends RigidBody3D
 
-@export var sideways_force := 100.0
+@export var sideways_force := 10.0
+
+var _initial_push := true
 
 func _ready() -> void:
 	contact_monitor = true
 	max_contacts_reported = 4
 	body_entered.connect(_on_body_entered)
-	# Initial push
-	apply_central_impulse(Vector3(-sideways_force * 0.06, -10, 0))
+
+func _physics_process(_delta: float) -> void:
+	if _initial_push:
+		_initial_push = false
+		linear_velocity = Vector3(-sideways_force, -10, 0)
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("ball_reflector"):
 		if body.global_position.x > global_position.x:
-			apply_central_impulse(Vector3(-sideways_force * 0.06, -10, 0))
+			linear_velocity = Vector3(-sideways_force, -10, 0)
 		else:
-			apply_central_impulse(Vector3(sideways_force * 0.06, -10, 0))
+			linear_velocity = Vector3(sideways_force, -10, 0)
