0530f3e37677b784c42482c1c41fef4eecf08452
gridlocdev <gridlocdev@fastmail.com>
2026-03-14 16:52:20 -0700

    fade in credits menu

--- godot-version/scripts/credits_bonus.gd
+++ godot-version/scripts/credits_bonus.gd
@@ -2,6 +2,9 @@ extends Control
 
 func _ready() -> void:
 	MusicManager.play_title_music()
+	modulate.a = 0.0
+	var tween := create_tween()
+	tween.tween_property(self, "modulate:a", 1.0, 1.0)
 
 func _on_menu_pressed() -> void:
 	GameManager.load_main_menu()
